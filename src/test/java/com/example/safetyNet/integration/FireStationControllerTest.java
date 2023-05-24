8e70fbdc38b4408f9f34da91b9bb034d83a4b035
Jamaa <saruul.jamaa@gmail.com>
2023-05-24 21:35:31 +0200

    test

--- src/test/java/com/example/safetyNet/integration/FireStationControllerTest.java
+++ src/test/java/com/example/safetyNet/integration/FireStationControllerTest.java
@@ -1,22 +1,19 @@
-package com.example.safetyNet.controller;
+package com.example.safetyNet.integration;
 
 
 import com.example.safetyNet.model.FireStation;
-import com.example.safetyNet.service.FireStationService;
+import com.example.safetyNet.repository.FireStationRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.Arrays;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,90 +21,73 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 class FireStationControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//    @Autowired
-//    private FireStationService fireStationService;
-//
-//    private final String url = "/firestation";
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    // @Test
-//    void shouldAddFireStationAndReturn201WhenValidInput() throws Exception {
-//        //GIVEN
-//        FireStation fireStation = new FireStation("1", "Test");
-//
-//        //WHEN
-//        //when(fireStationServiceImpl.ajouter(any(FireStation.class))).thenReturn(
-//        //Arrays.asList(fireStation));
-//        MvcResult mvcResult = this.mockMvc.perform(
-//                        post(url).contentType("application/json")
-//                                .content(objectMapper.writeValueAsString(fireStation)))
-//                .andExpect(status().isCreated()).andReturn();
-//
-//        //THEN
-//        String result = mvcResult.getResponse().getContentAsString();
-//        assertEquals(objectMapper.writeValueAsString(Arrays.asList(fireStation)), result);
-//
-//    }
-//
-//    // @Test
-//    void shouldAddFireStationAndReturnWhenValidInput() throws Exception {
-//        //GIVEN
-//        FireStation fireStation = new FireStation();
-//
-//        //WHEN
-//        when(fireStationService.ajouter(any(FireStation.class))).thenReturn(
-//                Arrays.asList(fireStation));
-//        MvcResult mvcResult = this.mockMvc.perform(
-//                        post(url).contentType("application/json")
-//                                .content(objectMapper.writeValueAsString(fireStation)))
-//                .andExpect(status().isBadRequest()).andReturn();
-//
-//        //THEN
-//        String result = mvcResult.getResponse().getContentAsString();
-//        Pattern re = Pattern.compile("\\bstation|address\\b", Pattern.CASE_INSENSITIVE);
-//        assertEquals(true, re.matcher(result).find());
-//
-//    }
-//
-//    // @Test
-//    void shouldUpdateFireStationAndReturnWhenValidInput() throws Exception {
-//        //GIVEN
-//        FireStation fireStation = new FireStation("1", "1 Test");
-//
-//        //WHEN
-//        when(fireStationService.update(any(String.class),
-//                any(String.class))).thenReturn(
-//                fireStation);
-//        MvcResult mvcResult = this.mockMvc.perform(
-//                        put(url).param("address", "1 Test").contentType(
-//                                        "application" +
-//                                                "/json")
-//                                .content(objectMapper.writeValueAsString(fireStation)))
-//                .andExpect(status().isOk()).andReturn();
-//
-//        //THEN
-//        String result = mvcResult.getResponse().getContentAsString();
-//
-//        assertEquals(objectMapper.writeValueAsString(fireStation), result);
-//
-//    }
-//
-//    //@Test
-//    void shouldDeleteFireStationAndReturnValidInput() throws Exception {
-//
-//        //WHEN
-//        when(fireStationService.delete(any(String.class))).thenReturn(
-//                Arrays.asList(new FireStation()));
-//        MvcResult mvcResult = this.mockMvc.perform(
-//                        delete(url).param("address", "1 Test")
-//                )
-//                .andExpect(status().isNoContent()).andReturn();
-//
-//    }
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private FireStationRepository fireStationRepository;
+    private final String url = "/firestation";
+    @Autowired
+    private ObjectMapper objectMapper;
+    private FireStation fireStation = new FireStation();
+    private List<FireStation> fireStations = new ArrayList<>();
+
+    @BeforeEach
+    public void setUp() {
+        fireStation = new FireStation();
+        fireStation.setAddress("1 Test");
+        fireStation.setStation("1");
+        fireStations = fireStationRepository.getFireStationsList();
+    }
+
+    @Test
+    void shouldReturnFireStation() throws Exception {
+        this.mockMvc.perform(
+                        get("/firestation/address").param("address", "1509 Culver St")
+                                .contentType("application/json"))
+                .andExpect(status().isOk()).andReturn();
+    }
+
+    @Test
+    void shouldAddFireStationAndReturn201WhenValidInput() throws Exception {
+        this.mockMvc.perform(
+                        post(url).contentType("application/json")
+                                .content(objectMapper.writeValueAsString(fireStation)))
+                .andExpect(status().isCreated()).andReturn();
+    }
+
+    @Test
+    void shouldAddFireStationAndReturnWhenValidInput() throws Exception {
+        FireStation fireStation = new FireStation();
+        this.mockMvc.perform(
+                        post(url).contentType("application/json")
+                                .content(objectMapper.writeValueAsString(fireStation)))
+                .andExpect(status().isBadRequest()).andReturn();
+
+
+    }
+
+    @Test
+    void shouldUpdateFireStationAndReturnWhenValidInput() throws Exception {
+        fireStations.add(fireStation);
+        this.mockMvc.perform(
+                        put(url).param("address", "1 Test").contentType(
+                                        "application" +
+                                                "/json")
+                                .content(objectMapper.writeValueAsString(fireStation)))
+                .andExpect(status().isOk()).andReturn();
+
+    }
+
+    @Test
+    void shouldDeleteFireStationAndReturnValidInput() throws Exception {
+        fireStations.add(fireStation);
+        this.mockMvc.perform(
+                        delete(url).param("address", "1 Test")
+                )
+                .andExpect(status().isNoContent()).andReturn();
+
+    }
 
 
 }
